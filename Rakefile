2b26d432f4fd78205915c1edad5a0a2a76497565
scott m parrish <anithri@gmail.com>
2016-05-01 14:00:13 -0600

    Little playing about and tuning for my RubyMine IDE

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@ require 'squib'
 
 task default: [:deck]
 
+desc 'Generate a deck by running deck.rb'
 task :deck do
   load 'deck.rb'
 end
--- deck.rb
+++ deck.rb
@@ -1,6 +1,11 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+Squib::Deck.new cards: 6, layout: 'economy.yml' do
+  background color: 'white'
+  rect layout: 'cut' # die cut line as defined by TheGameCrafter
+  rect layout: 'safe' # safe zone as defined by TheGameCrafter
+  text str: ['Bruce', 'Tim', 'Barbara', 'Alfred', 'James', 'Selina'].shuffle,
+       layout: 'title'
+  text str: "Draw #{rand(1..5)} cards.", layout: 'description'
+  save_png
 end
